8b778e609cc5773c7b410f39518c85fcdd4d702f
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-03-03 13:47:07 +0100

    Set up models and data manager structure

---
+++ models.py
@@ -0,0 +1,30 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    movies = db.relationship('Movie', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"<User {self.name}>"
+
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(255), nullable=False)
+    director = db.Column(db.String(150))
+    year = db.Column(db.Integer)
+    poster_url = db.Column(db.String(500))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+
+    def __repr__(self):
+        return f"<Movie {self.title} (User ID: {self.user_id})>"
